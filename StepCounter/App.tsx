6505f5c74a346a5f53ffb3e58de64b4352829089
Hirata, Yudai <yuhirata@iu.edu>
2024-11-08 10:11:39 -0500

    Add Apple HealtKit basic app

---
+++ StepCounter/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Value from './src/components/Value';
+import RingProgress from './src/components/RingProgress';
+import useHealthData from './src/components/hooks/useHealthData';
+
+
+export default function App() {
+  const {steps, distance, flights} = useHealthData();
+
+	console.log(`Steps: ${steps} | Distance: ${distance}m | Flights: ${flights}`);
+  return (
+    <View style={styles.container}>
+      <RingProgress progress={ steps / 10000 } />
+      <View style={styles.values}>
+        <Value label="Steps" value={steps.toString()} />
+        <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`} />
+        <Value label="Flights Climbed" value={flights.toString()} />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    padding: 12,
+  },
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
+  }
+});
